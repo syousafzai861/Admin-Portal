8285f91ef3d79a75f2e66d12670efd5c31a09424
Saif Ul islam <91761499+syousafzai861@users.noreply.github.com>
2023-01-12 17:00:34 +0500

    Admin portal
    iniatlized the data bases and user roles in the backed on strapi

---
+++ myAdmin/config/database.js
@@ -0,0 +1,22 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "Admin"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "saif@006"),
+      schema: env("DATABASE_SCHEMA", "public"),
+    },
+  }
+});
+  
+   
+
+
+
+
+
+
